d6e53bcb8241522e452d5513c1fde8294328746f
Jennifer <jennifer881030@gmail.com>
2023-04-01 21:02:16 +0800

    Design Forum page layout

--- src/pages/Forum/Forum.tsx
+++ src/pages/Forum/Forum.tsx
@@ -1,8 +1,113 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 100%;
+  padding: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+
+const WelcomeMessage = styled.div`
+  font-size: 22px;
+  font-weight: 700;
+  text-align: center;
+  margin-bottom: 10px;
+`;
+
+const SearchBar = styled.input`
+  border-radius: 5px;
+  border: #b6b6b6 solid 1px;
+  height: 30px;
+  width: 100%;
+  padding: 10px;
+`;
+
+const Boards = styled.div`
+  display: flex;
+  margin-top: 40px;
+  gap: 30px;
+  font-weight: 500;
+  align-items: center;
+`;
+
+const Articles = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  margin-bottom: 80px;
+`;
+
+const Article = styled.div`
+  background-color: #b8b8b8;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 14px;
+  height: 60px;
+`;
+
+const Title = styled.div`
+  text-align: left;
+  font-weight: 700;
+`;
+
+const Pagination = styled.div`
+  color: #000;
+  font-weight: 700;
+  margin-top: 20px;
+  text-align: center;
+`;
+
 function Forum() {
+  const BoardData = {
+    type: ['台劇版', '韓劇版', '動畫版', '美劇版'],
+  };
+
   return (
-    <>
-      <h1>Forum</h1>
-    </>
+    <Wrapper>
+      <WelcomeMessage>歡迎來到 4th forum 一起討論戲劇！</WelcomeMessage>
+      <SearchBar type="text" placeholder="請輸入想要查找的文章標題" />
+      <Boards>
+        {BoardData.type.map((type) => {
+          return <div>{type}</div>;
+        })}
+      </Boards>
+      <hr className="my-4" />
+      <Articles>
+        <Article>
+          <div>28</div>
+          <Title>[閒聊] 黑暗榮耀的女主角手法好像某個人</Title>
+          <div>ffuri</div>
+          <div>3/26</div>
+        </Article>
+        <Article>
+          <div>72</div>
+          <Title>[徵文] 2016 明星韓劇-孤單又燦爛的神 鬼怪</Title>
+          <div>fifi</div>
+          <div>3/25</div>
+        </Article>
+        <Article>
+          <div>72</div>
+          <Title>[心得] 重看2521才是正確打開這部韓劇的方式</Title>
+          <div>tftt</div>
+          <div>3/24</div>
+        </Article>
+        <Article>
+          <div>89</div>
+          <Title>[新聞] 李到晛鬆口談《黑暗榮耀》第三季去向！</Title>
+          <div>jinnit</div>
+          <div>3/24</div>
+        </Article>
+        <Article>
+          <div>💥</div>
+          <Title>[LIVE] Law School/法學院 EP08</Title>
+          <div>kkuri</div>
+          <div>3/22</div>
+        </Article>
+        <Pagination>◀ 1 2 3 4 ▶</Pagination>
+      </Articles>
+    </Wrapper>
   );
 }
 
