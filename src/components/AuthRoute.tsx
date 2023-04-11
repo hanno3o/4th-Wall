9286dde121e830cbd7b554a5ab6848e5f6189612
Jennifer <jennifer881030@gmail.com>
2023-04-11 11:28:36 +0800

    Add user info to Firestore if not exists

--- src/components/AuthRoute.tsx
+++ src/components/AuthRoute.tsx
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { db } from '../config/firebase.config';
 import { useNavigate } from 'react-router-dom';
 
 export interface AuthRouteProps {
   children?: React.ReactNode;
+}
+interface IUserInfo {
+  email?: string | null;
+  username?: string | null;
+  avatar?: string | null;
 }
 
 const AuthRoute: React.FunctionComponent<AuthRouteProps> = (props) => {
@@ -11,10 +18,26 @@ const AuthRoute: React.FunctionComponent<AuthRouteProps> = (props) => {
   const auth = getAuth();
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
+  const [userInfo, setUserInfo] = useState<IUserInfo>();
 
   useEffect(() => {
-    const AuthCheck = onAuthStateChanged(auth, (user) => {
+    const AuthCheck = onAuthStateChanged(auth, async (user) => {
       if (user) {
+        const userRef = doc(db, 'users', user.uid);
+        const userDoc = await getDoc(userRef);
+        if (userDoc.exists()) {
+          const userData = userDoc.data();
+          setUserInfo(userData);
+        } else {
+          const userData = {
+            email: user.email,
+            username: user.displayName,
+            avatar: user.photoURL,
+          };
+
+          await setDoc(userRef, userData);
+          setUserInfo(userData);
+        }
         setIsLoading(false);
       } else {
         console.log('unauthorized');
