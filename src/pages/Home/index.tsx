0479d254bbb640c350590e2de4e8b6012e98b3bf
Jennifer <jennifer881030@gmail.com>
2023-06-03 14:07:00 +0800

    Refactor: Move filterData to constants file and rename firebase.config.tsx to firebase.config.ts

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,6 +8,7 @@ import Dramas from '../../components/Dramas';
 import { useAppSelector, useAppDispatch } from '../../redux/hooks';
 import { GET_ALL_DRAMAS } from '../../redux/reducers/dramasSlice';
 import { debounce } from '../../utils/debounce';
+import { filterData } from '../../utils/constants';
 
 const MEDIA_QUERY_TABLET =
   '@media screen and (min-width: 1281px) and (max-width: 1440px)';
@@ -109,53 +110,6 @@ interface MultiFilterOptionProps {
 }
 
 export default function Home() {
-  const filterData = {
-    type: ['所有影集', '台劇', '韓劇', '日劇', '美劇', '陸劇'],
-    filters: [
-      {
-        title: '類型',
-        filter: [
-          '愛情',
-          '喜劇',
-          '劇情',
-          '奇幻',
-          '穿越',
-          '懸疑',
-          '校園',
-          '刑偵犯罪',
-          '復仇',
-          '律政',
-          '職場',
-          '醫療',
-          '音樂',
-          '時代',
-          '古裝',
-        ],
-      },
-      {
-        title: '排序',
-        filter: ['新上架', '評價最高', '由新到舊', '由舊到新'],
-      },
-      {
-        title: '年份',
-        filter: [
-          2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013,
-          2012, 2011, 2010,
-        ],
-      },
-      {
-        title: '平台',
-        filter: [
-          'Netflix',
-          'Disney+',
-          'LINE TV',
-          '愛奇藝',
-          'Friday影音',
-          'KKTV',
-        ],
-      },
-    ],
-  };
   const [searchWords, setSearchWords] = useState('');
   const [selectedTypeFilter, setSelectedTypeFilter] = useState<string | null>(
     '所有影集'
